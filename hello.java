52aef4a13ef8d92a66c943541cc4719ef57267e7
B_joy <bijoyprofessional@gmail.com>
2021-07-03 00:35:02 +0600

    Added a new method named Greetings to hello.java

--- hello.java
+++ hello.java
@@ -1,6 +1,13 @@
 public class hello {
     public static void main(string[] args){
 
-        system.out.println("vs code is awesome")
+        system.out.println("vs code is awesome");
+
+        greetings("Bijoy");
+    }
+
+    public static void greetings(string name) {
+
+        system.out.println("Wellcome" + name);
     }
 }
